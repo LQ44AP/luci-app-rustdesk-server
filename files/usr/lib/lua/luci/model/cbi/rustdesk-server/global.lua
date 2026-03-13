fb911f473915778188b2bee34f1bd0bb3304c086
99010 <11658987+lfznnzq@users.noreply.github.com>
2026-03-13 19:34:24 +0800

    Delete files/usr/lib/lua/luci/model/cbi/rustdesk-server/global.lua

--- files/usr/lib/lua/luci/model/cbi/rustdesk-server/global.lua
+++
@@ -1,9 +0,0 @@
-local m = Map("rustdesk-server", translate("RustDesk 服务器全局设置"))
-local s = m:section(NamedSection, "global", "global", translate("基础配置"))
-
-local data_dir = s:option(Value, "data_dir", translate("数据目录"))
-data_dir.description = translate("存放 ID/Key 文件的目录")
-data_dir.default = "/root/.rustdesk-server"
-data_dir.rmempty = false
-
-return m
